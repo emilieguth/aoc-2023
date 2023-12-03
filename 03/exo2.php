69b89ec7c63b1100767de855a4a93dde201cbcc5
emilieguth <emilie.guth@zenride.co>
2023-12-03 21:00:33 +0100

    🧹Clean

--- 03/exo2.php
+++ 03/exo2.php
@@ -5,59 +5,57 @@ $lines = extractFileToArray('input.txt');
 
 $input2DimArray = convertInputTo2DimArray($lines);
 
-$xSurrendingNumbers = [];
+$xSurroundingNumbers = [];
 $partNumbers = [];
 $isPartNumber = false;
-$xPositions = [];
 
 foreach ($input2DimArray as $line => $cells) {
   $currentNumber = '';
 
   foreach ($cells as $row => $cell) {
     if ($cell === '*') {
-      $xPositions[] = "$line;$row";
-      $xSurrendingNumbers[$line][$row] = [];
+      $xSurroundingNumbers[$line][$row] = [];
       [$i, $j, $value] = getNumber($input2DimArray, $line - 1, $row - 1);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
       [$i, $j, $value] = getNumber($input2DimArray, $line - 1, $row);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
       [$i, $j, $value] = getNumber($input2DimArray, $line - 1, $row + 1);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
       [$i, $j, $value] = getNumber($input2DimArray, $line, $row - 1);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
       [$i, $j, $value] = getNumber($input2DimArray, $line, $row + 1);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
       [$i, $j, $value] = getNumber($input2DimArray, $line + 1, $row - 1);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
       [$i, $j, $value] = getNumber($input2DimArray, $line + 1, $row);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
       [$i, $j, $value] = getNumber($input2DimArray, $line + 1, $row + 1);
       if ($i !== null and $j !== null) {
-        $xSurrendingNumbers[$line][$row]["$i;$j"] = (int)$value;
+        $xSurroundingNumbers[$line][$row]["$i;$j"] = (int)$value;
       }
     }
   }
 }
 
 $numbers = [];
-foreach($xSurrendingNumbers as $line => $xSurrendingNumbersByLine) {
-  foreach($xSurrendingNumbersByLine as $row => $xSurrendingNumbersByRow) {
-    if(count($xSurrendingNumbersByRow) === 2) {
-      $numbers[] = array_product($xSurrendingNumbersByRow);
+foreach($xSurroundingNumbers as $line => $xSurroundingNumbersByLine) {
+  foreach($xSurroundingNumbersByLine as $row => $xSurroundingNumbersByRow) {
+    if(count($xSurroundingNumbersByRow) === 2) {
+      $numbers[] = array_product($xSurroundingNumbersByRow);
     }
   }
 }
