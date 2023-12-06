17ca975b7d7654de3a9bd2b395d60c21f8604f37
emilieguth <emilie.guth@zenride.co>
2023-12-06 07:34:24 +0100

    🎄 Day 6 - part 2

---
+++ 06/exo2.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/../utils.php';
+$lines = extractFileToArray('input.txt');
+var_dump($lines);
+$winningChoices = 0;
+
+$time = extractData($lines[0], 5);
+$distance = extractData($lines[1], 10);
+
+for($ms = 0; $ms < $time; $ms++) {
+  $myDistance = max(0, ($time - $ms)) * $ms;
+  if ($myDistance > $distance) {
+    $winningChoices++;
+  }
+}
+var_dump($winningChoices);
+
+function extractData(string $line, int $offset): int {
+  return (int)str_replace(' ', '', substr($line, $offset));
+}
